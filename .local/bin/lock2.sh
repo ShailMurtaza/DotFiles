346c1813fb63507e07780d7e8ee15fce08786325
ShailMurtaza <shailmurtaza@gmail.com>
2026-01-31 23:17:15 +0500

    lock2.sh

---
+++ .local/bin/lock2.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+i3lock -k -L \
+-i /home/shail/Pictures/backgrounds/parrot-wallpapers/backgrounds/lorikeet.jpg \
+--ring-color=003E48 \
+--inside-color=281214b3 \
+--line-color=ffffff \
+--insidewrong-color=FF000E \
+--ringwrong-color=ffffff \
+--ring-width=10 \
+--insidever-color=3465A4 \
+--indicator \
+--verif-color=BABDB6 \
+--time-color=ffffff \
+--date-color=ffffff \
+--time-str="%I:%M:%S %p" \
+--date-str="%a, %d %b(%m) %Y" \
+--radius=120
